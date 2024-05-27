cfb80510755bc20aa4e8a25f5533caf3eb556268
tj369369 <1512594375@qq.com>
2024-05-27 18:36:53 +0800

    test1-master

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * @Author：tj
+ * @Package：org.example
+ * @Project：${PROJECT_NAME}
+ * @name：${NAME}
+ * @Date：${DATE} ${TIME}
+ * @Filename：${NAME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! test2-branch1");
+    }
+}
